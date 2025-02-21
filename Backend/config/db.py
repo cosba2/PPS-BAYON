6ec03e108e7643675778968debc2ac65bef975cc
cosba2 <marcosbayon22@gmail.com>
2025-02-21 10:01:21 -0300

    preparop

--- Backend/config/db.py
+++ Backend/config/db.py
@@ -1,15 +1,9 @@
+from dotenv import load_dotenv
 import os
-from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+load_dotenv()  # Carga las variables desde el archivo .env
 
-DB_USERNAME = os.getenv("DB_USERNAME")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT", "3306")
-DB_NAME = os.getenv("DB_NAME")
-
-if not all([DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME]):
-    raise ValueError("Faltan variables de entorno para la base de datos")
-
-SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = (
+    f"mysql+pymysql://{os.getenv('MYSQLUSER')}:{os.getenv('MYSQLPASSWORD')}"
+    f"@{os.getenv('MYSQLHOST')}:{os.getenv('MYSQLPORT')}/{os.getenv('MYSQLDATABASE')}"
+)
