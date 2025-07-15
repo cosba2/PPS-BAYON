77a1799177ae7df57a5e6d57ef87f558f2137e63
cosba2 <marcosbayon22@gmail.com>
2025-07-15 11:11:17 -0300

    modifico apikey

--- Backend/config/db.py
+++ Backend/config/db.py
@@ -5,6 +5,5 @@ db = SQLAlchemy()
 
 def init_app(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "postgresql://bdd_pps_user:aV4ajhEnqCkuiIdX9KXhHqXXLe1HKs1d@dpg-cvsjqnbuibrs73ehmsag-a/bdd_pps")
-    #app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "mysql+pymysql://root:18128993@localhost/blog_db")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
