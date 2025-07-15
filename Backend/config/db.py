1bd30f0d911a22584b69727e148deb0813ba980c
cosba2 <marcosbayon22@gmail.com>
2025-07-15 11:07:35 -0300

    elimino linea de db.py

--- Backend/config/db.py
+++ Backend/config/db.py
@@ -7,5 +7,4 @@ def init_app(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "postgresql://bdd_pps_user:aV4ajhEnqCkuiIdX9KXhHqXXLe1HKs1d@dpg-cvsjqnbuibrs73ehmsag-a/bdd_pps")
     #app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "mysql+pymysql://root:18128993@localhost/blog_db")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    API_KEY = os.getenv("X-API-KEY", "marcospps")
     db.init_app(app)
