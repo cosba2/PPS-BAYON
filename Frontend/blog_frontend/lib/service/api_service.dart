3d97732c305ec6d8c196fba1c8702a5191610291
cosba2 <marcosbayon22@gmail.com>
2025-07-21 11:33:10 -0300

    comienzo con nuevo Front

---
+++ Frontend/blog_frontend/lib/service/api_service.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:blog_frontend/models/user_model.dart';
+
+class ApiService {
+  static const String baseUrl = 'https://pps-bayon.onrender.com/api';
+  static const String apiKey = 'marcospps'; // ⚠️ reemplaza con tu API KEY real
+
+  static Future<List<User>> getUsers() async {
+    final response = await http.get(
+      Uri.parse('$baseUrl/users'),
+      headers: {
+        'Content-Type': 'application/json',
+        'X-API-KEY': apiKey,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      List<dynamic> body = json.decode(response.body);
+      List<User> users = body.map((json) => User.fromJson(json)).toList();
+      return users;
+    } else {
+      throw Exception('Error al cargar usuarios: ${response.body}');
+    }
+  }
+}
